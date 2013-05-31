bc5562190731b195e5cdae5b62a575c978fd0de6
sergio <sergio.canis@outlook.com>
2013-05-31 12:18:45 +0200

    Primer commit. Aplicación en pañales

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PiensaQue::Application.config.secret_token = '6a75cd9c40858c4576c56a5d621eb82de8752e351dd7f541943c464608d82abb853a40fbc2d9264fbd427bd553058414024cbbc6c47ae9ed8bff578633205e0e'
