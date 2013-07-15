1c600d71f1fcb1ceb830225219ec35a06972e886
sergio <sergio.canis@outlook.com>
2013-07-15 20:11:36 +0200

    Añadido Rango

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,22 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :nick, :password
+  attr_accessible :email, :rank, :nick, :password
+
+	before_save :assign_rank
+
+
+	# Método para asignar permisos
+	# BIG_BOSS 	- Nivel alto
+	# ADMIN 		- Nivel medio
+	# DEFAULT 	- Nivel bajo
+	def assign_rank(options={})
+		user = user.find(options[:id])
+		if options.blank?
+			user.update_attributes(:rank => 'default')
+		elsif (options[:rank] == "BIG BOSS")
+			user.update_attributes(:rank => options[:rank])
+		end
+			
+
+	end
+
 end
