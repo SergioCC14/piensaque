d0e908108df522d4fbc4e8c8693db890077b010f
sergio <sergio.canis@outlook.com>
2013-07-15 20:33:23 +0200

    Empezando sesiones

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-PiensaQue::Application.config.session_store :cookie_store, key: '_PiensaQue_session'
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# PiensaQue::Application.config.session_store :active_record_store
+PiensaQue::Application.config.session_store :active_record_store, {
+  :domain => :all,
+  :expire_after => 36.hours,
+  :key => '_PiensaQue_id'
+}
