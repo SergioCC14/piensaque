1f114986153e3f9de6cec1c5129b124b044c8c89
sergio <sergio.canis@outlook.com>
2013-08-20 17:37:37 +0200

    Prueba3

--- app/models/paperclip.rb
+++ app/models/paperclip.rb
@@ -1,2 +1,2 @@
-Paperclip::Attachment.default_options[:url] = 's3-eu-west-1.amazonaws.com/'
+Paperclip::Attachment.default_options[:url] = ':s3_domain_path'
 Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
