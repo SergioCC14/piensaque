a6905195ffb54063603958ea55e8707bab13fe19
sergio <sergio.canis@outlook.com>
2013-08-20 21:39:13 +0200

    Bugfix2

--- app/models/paperclip.rb
+++ app/models/paperclip.rb
@@ -1,2 +1,2 @@
-# Paperclip::Attachment.default_options[:url] = ':s3_domain_path'
-# Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+Paperclip::Attachment.default_options[:url] = ':s3_domain_path'
+Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
