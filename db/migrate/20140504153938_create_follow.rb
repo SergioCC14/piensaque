b4521c2347666991153c2210faef1d49d3915616
SergioCC14 <sergio.canis@outlook.com>
2014-05-11 22:14:57 +0200

    -1

--- db/migrate/20140504153938_create_follow.rb
+++ db/migrate/20140504153938_create_follow.rb
@@ -1,15 +1,12 @@
 class CreateFollow < ActiveRecord::Migration
   def change
     
-
     create_table :relations do |t|
       t.integer :user_id
       t.integer :user_relation_id
 
       t.timestamps
     end
-    add_column :users, :followers_count, :integer, :default => 0
-    add_column :users, :following_count,  :integer, :default => 0
 
   end
 end
