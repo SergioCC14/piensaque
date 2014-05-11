78d0d72af1d09940afb746e8445131b8ecc57fb4
SergioCC14 <sergio.canis@outlook.com>
2014-05-11 22:05:42 +0200

    ++

--- db/migrate/20130909171039_add_followers_following_to_users.rb
+++ db/migrate/20130909171039_add_followers_following_to_users.rb
@@ -1,11 +1,15 @@
 class AddFollowersFollowingToUsers < ActiveRecord::Migration
-  def up
+  def change
+
+    create_table :relations_tables do |t|
+      t.integer :user_id
+      t.integer :user_relation_id
+
+      t.timestamps
+    end
+
     add_column :users, :followers, :integer, :default => 0
     add_column :users, :following,  :integer, :default => 0
   end
 
-  def down
-    remove_column :users, :followers
-    remove_column :users, :following
-  end
 end
