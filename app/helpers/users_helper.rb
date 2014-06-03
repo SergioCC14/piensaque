fb899623c7590ca56d6a6639b4c60a2778c04477
SergioCC14 <sergio.canis@outlook.com>
2014-06-03 21:01:44 +0200

    X, X, X también estan en PnsQ...

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -15,4 +15,34 @@ module UsersHelper
     end
   end
 
+  # Busca entre tres usuarios (aleatoriamente) en PiensaQue y devuelve los nicks
+  def some_users
+    users = []
+    user_ids = User.ids.sort_by{rand}[1..3]
+    
+    for id in user_ids
+      user = User.find_by(id: id)
+
+      users << "<a href=#{user_nick_path(user.nick)}> #{name_or_nick(user)}</a>"
+    end
+
+    return users.join(", ")
+  end
+
+  # Devuelve el nombre, si no tiene, el nick
+  def name_or_nick(user, surname=false)
+    value = ""
+
+    if (!user.name.blank?)
+      if !user.name.blank?
+        value = (!user.surname.blank? and surname) ? "#{user.name} #{user.surname}" : user.name 
+      end
+
+    else
+      value = user.nick
+    end
+
+    return value
+  end
+
 end
