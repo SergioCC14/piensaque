b1707ed649e2b89262c2d033d4e0579745764216
SergioCC14 <sergio.canis@outlook.com>
2014-06-03 08:55:40 +0200

    UserNextStep, preset

--- app/models/user_next_step.rb
+++ app/models/user_next_step.rb
@@ -2,9 +2,17 @@ class UserNextStep < ActiveRecord::Base
 
   STEPS = ['dashboard_help']
 
+  after_create :preset
+
   # Comprueba si ese paso existe en el tutorial
   def self.exist_step?(step)
     return STEPS.include?(step)
+  end
+
+  def preset
+    self.assign_attributes(
+      :dashboard_help => false,
+      :user_show_help => false)
   end
 
 end
